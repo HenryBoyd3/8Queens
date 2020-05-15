85b0b896575c41c38cb94e18ed251c190df5b7bb
Dorkwoodgames <dorkwoodgames@gmail.com>
2020-05-14 19:41:54 -0700

    Added a way to take moves back if not wanted. This will set most of the game up for the algorithm to run and be able to back track

--- 8Queens/Form1.cs
+++ 8Queens/Form1.cs
@@ -14,6 +14,7 @@ namespace _8Queens
     {
         Button[,] Spaces = new Button[8, 8];
         Location queenplace;//where algorithm lives
+        int queen = 0;
         public Form1()
         {
            InitializeComponent();
@@ -30,12 +31,69 @@ namespace _8Queens
         {
 
         }
-        private void placeQueens(int x,int y) 
-        {
 
-            setMap(x,y); 
+        private void reset()
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                for (int row = 0; row < 8; row++)
+                {
+                    if ((row + col) % 2 == 0)
+                        Spaces[col, row].BackColor = Color.DarkGray;
+                    else
+                    {
+                        Spaces[col, row].BackColor = Color.White;
+                    }
+                    Spaces[col, row].Tag = "empty";
+                }
+
+            }
         }
-        
+        private void setBoard()
+        {
+            for (int col = 0; col < 8; col++)
+            {
+                for (int row = 0; row < 8; row++)
+                {
+
+                    Spaces[col, row] = new Button();
+                    Spaces[col, row].Size = new Size(44, 44);
+                    Spaces[col, row].Name = "" + col  + "" + row + "";
+                    Spaces[col, row].Text = "" + (col+1) + "," + (row + 1) + "";
+                    if ((row + col) % 2 == 0)
+                        Spaces[col, row].BackColor = Color.DarkGray;
+                    else
+                    {
+                        Spaces[col, row].BackColor = Color.White;
+                    }
+                    Spaces[col, row].Click += new EventHandler(testClick);
+                    Spaces[col, row].Tag = "empty";
+                    newgrid.Controls.Add(Spaces[col, row]);
+                }
+            }
+        }
+        private void testClick(object sender, EventArgs e)
+        {
+            Button test = sender as Button;
+            int y = (test.Location.X - 3) / 50;
+            int x = (test.Location.Y - 3) / 50;
+            if (Spaces[x, y].Tag.ToString() == "empty")
+            {
+                Spaces[x, y].Tag = "queen" + queen;
+                queen++;
+                setMap(x, y);
+                
+            }
+            else if ((Spaces[x, y].Tag.ToString() == "queen"))
+            {
+                Spaces[x, y].BackColor = Color.Yellow;
+                Spaces[x, y].Tag = "empty";
+                resetMap();
+                queen--;
+            }
+                
+        }
+
          private void setMap(int col, int row)
          {
             blockRow(col, row);
@@ -78,7 +136,6 @@ namespace _8Queens
                 dCol = dCol - 1;
                 dRow = dRow - 1;
                 blocks++;
-
             }
             }
          private void blockUpRight(int col, int row)
@@ -97,7 +154,6 @@ namespace _8Queens
                 dCol = dCol - 1;
                 dRow = dRow + 1;
                 blocks++;
-
             }
             }
          private void blockDownLeft(int col, int row)
@@ -116,7 +172,6 @@ namespace _8Queens
                 dCol = dCol + 1;
                 dRow = dRow - 1;
                 blocks++;
-
             }
            }
          private void blockDownRight(int col, int row)
@@ -135,74 +190,44 @@ namespace _8Queens
                 dCol = dCol + 1;
                 dRow = dRow + 1;
                 blocks++;
-
             }
            }
          private void setQueen (int col, int row)
             {
-
                Spaces[col,row].Tag = "queen";
                Spaces[col, row].BackColor = Color.Blue;
             }
-         private void block(int col, int row)
+        private void block(int col, int row)
          {
             if ((!(col < 0 && row < 0) || !(col > 7 && row > 7))
                 && (Spaces[col, row].Tag.ToString() == "empty"))
             {
-                Spaces[col, row].Tag = "blocked";
+                Spaces[col, row].Tag = "blocked"+queen;
                 Spaces[col, row].BackColor = Color.Red;
             }
 
          }
-
-        private void setBoard()
+        private void resetMap()
         {
             for (int col = 0; col < 8; col++)
             {
                 for (int row = 0; row < 8; row++)
                 {
+                    if (((row + col) % 2 == 0) && (Spaces[col, row].Tag.ToString() == ("blocked" + queen)))
+                    {
+                        Spaces[col, row].BackColor = Color.DarkGray;
+                        Spaces[col, row].Tag = "empty";
+
+                    }
+                    else if (Spaces[col, row].Tag.ToString() == ("blocked" + queen))
+                    {
+                        Spaces[col, row].BackColor = Color.White;
+                        Spaces[col, row].Tag = "empty";
+                    }
                     
-                    Spaces[col,row]= new Button();
-                    Spaces[col, row].Size = new Size(44, 44);
-                    Spaces[col, row].Name = "" + row + "" + col + "";
-                    Spaces[col, row].Text = "" + (row + 1) + "," + col + "";
-                    if ((row + col) % 2 == 0)
-                        Spaces[col, row].BackColor = Color.DarkGray;
-                    else
-                    {
-                        Spaces[col, row].BackColor = Color.White;
-                    }
-                    Spaces[col, row].Click += new EventHandler(testClick);
-                    Spaces[col, row].Tag = "empty";
-                    newgrid.Controls.Add(Spaces[col, row]);
-                }
-            }
-        }   
-        private void reset() 
-        {
-            for (int col = 0; col < 8; col++)
-            {
-                for (int row = 0; row < 8; row++)
-                {
-                    if ((row + col) % 2 == 0)
-                        Spaces[col, row].BackColor = Color.DarkGray;
-                    else
-                    {
-                        Spaces[col, row].BackColor = Color.White;
-                    }
-                    Spaces[col, row].Click += new EventHandler(testClick);
-                    Spaces[col, row].Tag = "empty";
                 }
 
-            } 
-        }
-        private void testClick(object sender, EventArgs e)
-        {
-            Button test = sender as Button;
-            int y = (test.Location.X - 3) / 50;
-            int x = (test.Location.Y - 3) / 50;
-            if (Spaces[x, y].Tag.ToString() == "empty")
-                setMap(x, y);
+            }
         }
     }
 }
